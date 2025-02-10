1f01879968539315d1c3bbcbde393bbcd21e87e3
PaulHM856 <walterhernandez856@gmail.com>
2025-02-09 19:15:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Contraseña predefinida
-const CONTRASENA = "040225"; // Cambia esto por la contraseña que desees
+const CONTRASENA = "180524"; // Cambia esto por la contraseña que desees
 
 // Variable para almacenar la contraseña ingresada
 let contrasenaIngresada = "";
